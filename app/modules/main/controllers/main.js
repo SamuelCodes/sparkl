e245a630f6ee3b970e7c746d78bf853464200e4b
Sam Coles <sam.coles@giantquanta.com>
2015-05-14 22:50:58 -0400

    Add nav to onto browser

--- app/modules/main/controllers/main.js
+++ app/modules/main/controllers/main.js
@@ -2,6 +2,47 @@ define(function(require) {
   var module = require('module!@');
 
   module
+    .controller('main.controllers.main', [ '$scope', 'uiGridConstants', function($scope, uiGridConstants) {
+      angular.extend($scope, {
+        editor: null,
+        aceLoaded: function(editor) { $scope.editor = editor; },
+        outputData: { columnDefs: [], data: [], onRegisterApi: function(api) { $scope.gridApi = api; } },
+        ontologyData: { columnDefs: [], data: [], onRegisterApi: function(api) { $scope.ontologyGridApi = api; } },
+        getQueryLines: function() { return $scope.editor.session.doc.$lines; },
+        getQuery: function() { return $scope.getQueryLines().join(" "); },
+        setOutput: function(content) {
+          if(content.length) {
+            var colDefs = [];
+            for(var key in content[0]) { colDefs.push({ name: key, field: key + '.value' }); }
+            $scope.outputData = { "columnDefs": colDefs, "data": content };
+          }
+        },
+        run: function() {
+          var queryUrl = "http://dbpedia.org/sparql"
+          $.ajax({
+            dataType: "jsonp",
+            url: queryUrl,
+            data: $.param({ query: $scope.getQuery(), format: "json", Accept: 'application/sparql-results+json' }),
+            success: function(_data) {
+              $scope.setOutput(_data.results.bindings);
+              $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.ALL);
+              $scope.appendHistory($scope.getQueryLines());
+            }
+          });
+        },
+        endpoints: ['http://dbpedia.org/sparql'],
+        history: function() {
+          var history = localStorage.getItem("history") || [];
+          return history;
+        },
+        appendHistory: function(query) {
+          var history = localStorage.getItem("history") || [];
+          history.push(query);
+          localStorage.setItem("history", history);
+        }
+      });
+    }
+    ])
     .directive('ontology', function() {
       return {
         restrict: 'A',
@@ -67,45 +108,4 @@ define(function(require) {
         }
       }
     })
-    .controller('main.controllers.main', [ '$scope', 'uiGridConstants', function($scope, uiGridConstants) {
-      angular.extend($scope, {
-        editor: null,
-        aceLoaded: function(editor) { $scope.editor = editor; },
-        outputData: { columnDefs: [], data: [], onRegisterApi: function(api) { $scope.gridApi = api; } },
-        ontologyData: { columnDefs: [], data: [], onRegisterApi: function(api) { $scope.ontologyGridApi = api; } },
-        getQueryLines: function() { return $scope.editor.session.doc.$lines; },
-        getQuery: function() { return $scope.getQueryLines().join(" "); },
-        setOutput: function(content) {
-          if(content.length) {
-            var colDefs = [];
-            for(var key in content[0]) { colDefs.push({ name: key, field: key + '.value' }); }
-            $scope.outputData = { "columnDefs": colDefs, "data": content };
-          }
-        },
-        run: function() {
-          var queryUrl = "http://dbpedia.org/sparql"
-          $.ajax({
-            dataType: "jsonp",
-            url: queryUrl,
-            data: $.param({ query: $scope.getQuery(), format: "json", Accept: 'application/sparql-results+json' }),
-            success: function(_data) {
-              $scope.setOutput(_data.results.bindings);
-              $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.ALL);
-              $scope.appendHistory($scope.getQueryLines());
-            }
-          });
-        },
-        endpoints: ['http://dbpedia.org/sparql'],
-        history: function() {
-          var history = localStorage.getItem("history") || [];
-          return history;
-        },
-        appendHistory: function(query) {
-          var history = localStorage.getItem("history") || [];
-          history.push(query);
-          localStorage.setItem("history", history);
-        }
-      });
-    }
-    ]);
 });
